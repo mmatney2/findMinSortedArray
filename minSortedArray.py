06a3a695a5425a35d66bd2a27d92caa59c626d5a
Marquita Matney <marquita.matney@gmail.com>
2022-09-26 18:31:06 -0400

    first commit

---
+++ minSortedArray.py
@@ -0,0 +1,17 @@
+def findMin(nums):
+    res = nums[0]
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        if nums[l] < nums[r]:
+            res = min(res, nums[l])
+            break
+            
+        m = (l + r) // 2
+        res = min(res, nums[m])
+        if nums[m] >= nums[l]:
+            l = m + 1
+        else:
+            r = m - 1
+    return res
+print(findMin([4,5,6,1,2,3]))
